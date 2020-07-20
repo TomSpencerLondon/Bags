f8a1b60a483a6eab1c655702755a4f9f25c3db4f
Tom <thomas.spencer@codurance.com>
2020-07-20 08:00:28 +0100

    Add herbBag

--- src/main/java/com/codurance/Bags.java
+++ src/main/java/com/codurance/Bags.java
@@ -1,18 +1,28 @@
 package com.codurance;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Bags {
 
   private final List<Item> backpack = new ArrayList<>();
+  private List<Item> herbBag = new ArrayList<>();
 
   public void addToBackpack(Item item) {
-    backpack.add(item);
+    if (backpack.size() < 8) {
+      backpack.add(item);
+    } else {
+      herbBag.add(item);
+    }
+
   }
 
   public List<Item> getBackpack() {
     return backpack;
   }
 
+  public Collection<Item> getHerbBag() {
+    return herbBag;
+  }
 }
--- src/test/java/com/codurance/BagsShould.java
+++ src/test/java/com/codurance/BagsShould.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.Test;
 
 public class BagsShould {
 
+  private final Bags bags = new Bags();
+
   @Test
   void add_8_items_to_the_backpack() {
-    Bags bags = new Bags();
     bags.addToBackpack(new Item("Leather", Category.CLOTHES));
     bags.addToBackpack(new Item("Iron", Category.HERB));
     bags.addToBackpack(new Item("Copper", Category.METAL));
@@ -19,5 +20,22 @@ public class BagsShould {
     bags.addToBackpack(new Item("Copper", Category.METAL));
 
     assertEquals(8, bags.getBackpack().size());
+  }
+
+  @Test
+  void place_9th_item_in_a_new_bag() {
+    bags.addToBackpack(new Item("Leather", Category.CLOTHES));
+    bags.addToBackpack(new Item("Iron", Category.HERB));
+    bags.addToBackpack(new Item("Copper", Category.METAL));
+    bags.addToBackpack(new Item("Marigold", Category.HERB));
+    bags.addToBackpack(new Item("Wool", Category.HERB));
+    bags.addToBackpack(new Item("Gold", Category.METAL));
+    bags.addToBackpack(new Item("Silk", Category.CLOTHES));
+    bags.addToBackpack(new Item("Copper", Category.METAL));
+
+    bags.addToBackpack(new Item("Seaweed", Category.HERB));
+
+    assertEquals(8, bags.getBackpack().size());
+    assertEquals(1, bags.getHerbBag().size());
   }
 }
