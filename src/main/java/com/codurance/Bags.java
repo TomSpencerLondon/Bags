be2c8d8edc3f251eddf536985997362108a5d928
Tom <thomas.spencer@codurance.com>
2020-07-20 07:22:07 +0100

    Refactor

--- src/main/java/com/codurance/Bags.java
+++ src/main/java/com/codurance/Bags.java
@@ -1,16 +1,11 @@
 package com.codurance;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class Bags {
 
-  private final List<String> backpack;
-
-  public Bags() {
-    backpack = new ArrayList<String>();
-  }
+  private final List<String> backpack = new ArrayList<>();
 
   public void addToBackpack(String item) {
     backpack.add(item);
